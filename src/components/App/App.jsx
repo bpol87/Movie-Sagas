004568a83d19fe9fad0c4c5263e63943b0be176e
Ben Pollack <benpollack8723@gmail.com>
2024-08-22 17:35:00 -0500

    styling and getting database set up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,10 @@
 import { Route, HashRouter as Router } from 'react-router-dom';
 import MovieList from '../MovieList/MovieList';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 >The Movies Saga!</h1>
+      <h1 className='text-center text-4xl p-6 bg-black text-white'>The Movies Saga!</h1>
       <Router>        
         <Route path="/" exact>
           <MovieList />
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -12,14 +12,14 @@ function MovieList() {
   }, []);
 
   return (
-    <main>
-      <h1>MovieList</h1>
-      <section className="movies">
+    <main className='pt-4 flex flex-col items-center'>
+      <h1 className='text-center text-3xl'>MovieList</h1>
+      <section className="flex flex-wrap items-center">
         {movies.map(movie => {
           return (
-            <div data-testid='movieItem' key={movie.id}>
-              <h3>{movie.title}</h3>
-              <img src={movie.poster} alt={movie.title}/>
+            <div data-testid='movieItem' key={movie.id} className='border-gray-700 border m-4 flex flex-col w-3/12 space-y-4'>
+              <h3 className='text-center p-2'>{movie.title}</h3>
+              <img src={movie.poster} alt={movie.title} />
             </div>
           );
         })}
